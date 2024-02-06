76b796d70c839d192664a0969e4a5baf65c2399e
Suwanruch-ARC <suwanruchs@arcinnovative.com>
2024-02-06 13:33:25 +0700

    แก้ไขการดึงข้อมูลจาก api

--- app/Http/Controllers/Api/DetailController.php
+++ app/Http/Controllers/Api/DetailController.php
@@ -53,7 +53,7 @@ class DetailController extends Controller
             $config = json_decode($campaign->settings);
             switch ($campaign->template_type) {
                 case 'STD':
-                    $logo = Image::getUrl($privilege->owner_id,'departments','logo');
+                    $logo = Image::getUrl($privilege->campaign->owner_id,'departments','logo');
                     $banner = Image::getUrl($privilege->id, 'privileges', 'banner') ?? Image::getUrl($privilege->shop->id, 'shops', 'banner');
                     $shop = $privilege->shop;
                     if ($now <= $user->expire_date) {
